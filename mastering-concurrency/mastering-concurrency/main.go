bc8ff40c2dd10f8244b322b9a9633b7c7cb54679
S.K.Venkat <1156760+skvenkat@users.noreply.github.com>
2023-02-20 22:45:48 +0000

    GO_misc_mastering_concurrency: Added the example implementation of Part2

---
+++ mastering-concurrency/mastering-concurrency/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"mastering-concurrency/part1"
+	"mastering-concurrency/part2"
+)
+
+func main() {
+	fmt.Println("~~~ Mastering Concurrency by Kyle Felter ~~~")
+	part1.Part1()
+	part1.Part2()
+}
