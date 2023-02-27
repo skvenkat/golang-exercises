f8dab53687cc572c07ec54c82c22c77b09a31e52
S.K.Venkat <1156760+skvenkat@users.noreply.github.com>
2023-02-27 22:48:05 +0000

    GO_misc_ex17: Implementation of server using various web frameworks (WIP)

---
+++ ex17/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	HOST = "0.0.0.0"
+	PORT = 8080
+)
+
+var ADDR_STR = fmt.Sprintf("%s:%d", HOST, PORT)
+
+
+
+func DefaultHTTP() {
+	fmt.Println("=== net/http server ===")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from Golang Inbuilt HTTP Server !!!"))
+	})
+
+	srv := &http.Server{
+		Addr: ADDR_STR,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+
+func EchoServer() {
+	fmt.Println("=== echo framework ===")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello from echooooo!!!")
+	})
+	e.Logger.Fatal(e.Start(ADDR_STR))
+}
+
+func ginRouter() *gin.Engine {
+	g := gin.Default()
+	g.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello from Gin!!!")
+	})
+	return g 
+}
+
+func GinServer() {
+	fmt.Println("=== gin framework ===")
+	r := ginRouter()
+	r.Run(ADDR_STR)
+}
+
+func GorillaMux() {
+	fmt.Println("=== gorilla mux framework ===")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from Gorilla MuX !!!"))
+	}).Methods("GET")
+	srv := &http.Server{
+		Handler: r,
+		Addr: ADDR_STR,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+func main() {
+	webserver := os.Args[1]
+	if webserver == "echo" {
+		EchoServer()
+	} else if webserver == "gin" {
+		GinServer()
+	} else if webserver == "gorilla" {
+		GorillaMux()
+	} else {
+		DefaultHTTP()
+	}
+}
