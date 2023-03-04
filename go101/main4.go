9e5cb67ab020a210c708654f8499c5ac949bd45c
S.K.Venkat <1156760+skvenkat@users.noreply.github.com>
2023-03-04 22:48:16 +0000

    GO_misc_ex: Go generics exercise ( WIP)

---
+++ go101/main4.go
@@ -0,0 +1,34 @@
+package main
+
+// NoDiff checks whether or not a collection
+// of values are all identical.
+func NoDiff[V comparable](vs ...V) bool {
+	if len(vs) == 0 {
+		return true
+	}
+	
+	v := vs[0]
+	for _, x := range vs[1:] {
+		if v != x {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var NoDiffString = NoDiff[string]
+	println(NoDiff("Go", "Go", "Go")) // true
+	println(NoDiffString("Go", "go")) // false
+	
+	println(NoDiff(123, 123, 123, 123)) // true
+	println(NoDiff[int](123, 123, 789)) // false
+	
+	type A = [2]int
+	println(NoDiff(A{}, A{}, A{}))     // true
+	println(NoDiff(A{}, A{}, A{1, 2})) // false
+	
+	println(NoDiff(new(int)))           // true
+	println(NoDiff(new(int), new(int))) // false
+
+	println(NoDiff[bool]())   // true
