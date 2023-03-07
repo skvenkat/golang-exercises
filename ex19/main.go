1ea6740129eaebd3995715a51163ee6dc46fae77
S.K.Venkat <1156760+skvenkat@users.noreply.github.com>
2023-03-07 22:00:36 +0000

    GO_misc_ex19: Speed up API implementation (WIP)

---
+++ ex19/main.go
@@ -0,0 +1,68 @@
+
+package handlers
+
+import (
+ "encoding/json"
+ "net/http"
+ "sync"
+)
+
+type Request struct {
+ Name    string `json:"name"`
+ Id      string `json:"id"`
+ Country string `json:"country"`
+ Age     int    `json:"age"`
+}
+
+type ObjectPool struct {
+ ReqPool *sync.Pool
+}
+
+var objPool ObjectPool
+
+func init() {
+ // init object pool
+ objPool.ReqPool = &sync.Pool{
+  New: func() interface{} {
+   return new(Request)
+  },
+ }
+
+}
+
+
+func HandlerWithoutSyncPool() http.Handler {
+ return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  req :=new(Request)
+  err := json.NewDecoder(r.Body).Decode(&req)
+  if err != nil {
+   w.WriteHeader(http.StatusBadRequest)
+   return
+  }
+  w.WriteHeader(http.StatusOK)
+  err = json.NewEncoder(w).Encode(req)
+  if err != nil {
+   w.WriteHeader(http.StatusInternalServerError)
+   return
+  }
+
+ })
+}
+
+func HandlerWithSyncPool() http.Handler {
+ return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  req := objPool.ReqPool.Get().(*Request)
+
+ //to make sure it return object to pool
+  defer objPool.ReqPool.Put(req)
+
+  err := json.NewDecoder(r.Body).Decode(&req)
+  if err != nil {
+   w.WriteHeader(http.StatusBadRequest)
+   return
+  }
+
+  w.WriteHeader(http.StatusOK)
+  
+ })
+}
