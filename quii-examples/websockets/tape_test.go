a837b71eb6a20221935cba551bd44a1699356c59
S.K.Venkat <1156760+skvenkat@users.noreply.github.com>
2023-05-18 23:57:42 +0100

    GO_misc_quii: added the test for the websockets example.

---
+++ quii-examples/websockets/tape_test.go
@@ -0,0 +1,35 @@
+package poker_test
+
+import (
+
+	"io"	"testing"
+
+	poker "github.com/quii/learn-go-with-tests/websockets/v2"
+
+)
+
+func TestTape_Write(t *testing.T) {
+
+	file, clean := createTempFile(t, "12345")
+
+	defer clean()
+
+	tape := &poker.Tape{File: file}
+
+	tape.Write([]byte("abc"))
+
+	file.Seek(0, 0)
+
+	newFileContents, _ := io.ReadAll(file)
+
+	got := string(newFileContents)
+
+	want := "abc"
+
+	if got != want {
+
+		t.Errorf("got %q want %q", got, want)
+
+	}
+
+}
